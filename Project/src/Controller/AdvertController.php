c6af75c3cbdec8c165faebb5823709018d2a4295
Melusine Chrs <melusine.chrs@gmail.com>
2024-10-01 14:03:04 +0200

    reset + base advert

---
+++ Project/src/Controller/AdvertController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Advert;
+use App\Form\AdvertType;
+use App\Repository\AdvertRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/advert')]
+final class AdvertController extends AbstractController
+{
+    #[Route(name: 'app_advert_index', methods: ['GET'])]
+    public function index(AdvertRepository $advertRepository): Response
+    {
+        return $this->render('advert/index.html.twig', [
+            'adverts' => $advertRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_advert_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $advert = new Advert();
+        $form = $this->createForm(AdvertType::class, $advert);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($advert);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_advert_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('advert/new.html.twig', [
+            'advert' => $advert,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_advert_show', methods: ['GET'])]
+    public function show(Advert $advert): Response
+    {
+        return $this->render('advert/show.html.twig', [
+            'advert' => $advert,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_advert_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Advert $advert, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AdvertType::class, $advert);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_advert_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('advert/edit.html.twig', [
+            'advert' => $advert,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_advert_delete', methods: ['POST'])]
+    public function delete(Request $request, Advert $advert, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$advert->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($advert);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_advert_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
